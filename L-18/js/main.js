ff7780cfaccf8861f7008d22ae7eab9105a13e38
gavrushoves <gavrushoves@list.ru>
2018-04-08 04:14:34 +0300

    L-18 advanced course

---
+++ L-18/js/main.js
@@ -0,0 +1,10 @@
+
+var main = function() {
+jQuery(function($){
+   $("#date").mask("99/99/9999");
+   $("#phone").mask("(999) 999-9999");
+   $("#tin").mask("99-9999999");
+   $("#ssn").mask("999-99-9999");
+});
+};
+$(document).ready(main); 
